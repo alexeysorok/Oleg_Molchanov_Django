6826dd5a0ecefa93b7eb4511df5e5ad81b3fc689
alexe <alexe@W10SSD>
2019-09-09 23:15:35 +0300

    Создание модели Tag, отношения ManyToMany

--- app/blogengine/blog/urls.py
+++ app/blogengine/blog/urls.py
@@ -5,5 +5,6 @@ from .views import *
 urlpatterns = [
     path('', post_list, name='posts_list_url'),
     path('post/<str:slug>/',post_detail, name='post_detail_url'), # угловые скобки означают именованные группы символов, нужно присвоить ей имя
+    path('tags/', tags_list, name='tags_list_url'),
 ]
 
--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
 from .models import *
 
-# Create your views here.
+
 def post_list(request):
     # n = 'Oleg'
     # list_names = ['Alex', 'Kostya', 'Yana']
-    posts = Post.objects.all() # получить все посты
+    posts = Post.objects.all()  # получить все посты
 
-    return render(request, 'blog/index.html', 
-        context={'posts': posts}) 
+    return render(request, 'blog/index.html',
+                  context={'posts': posts})
 
-def post_detail(request, slug): # slug придет из именнованой группы символов
+
+def post_detail(request, slug):  # slug придет из именнованой группы символов
     post = Post.objects.get(slug__iexact=slug)
-    return render(request, 'blog/post_detail.html', 
-            context={'post': post})
+    return render(request, 'blog/post_detail.html',
+                  context={'post': post})
+
+
+def tags_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'blog/tags_list.html', 
+                context={'tags': tags})
