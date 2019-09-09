807cb61961561a8eebf777524b20849f886f2380
alexe <alexe@W10SSD>
2019-09-09 19:04:30 +0300

    Вывод списка переменных

--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -3,5 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def post_list(request):
     n = 'Oleg'
+    list_names = ['Alex', 'Kostya', 'Yana']
     return render(request, 'blog/index.html', 
-        context={'name': n}) # name - будет использоваться в шаблоне
+        context={'names': list_names}) # name - будет использоваться в шаблоне
