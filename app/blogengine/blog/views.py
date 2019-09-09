0dcc2440eae4bb1e9ed9c3a2f9a61869df9a7c88
alexe <alexe@W10SSD>
2019-09-10 01:42:02 +0300

    Используем класс миксин

--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import *
 from django.views.generic import View
 
+from .utils import ObjectDetailMixin
+
 
 def post_list(request):
     # n = 'Oleg'
@@ -18,21 +20,26 @@ def tags_list(request):
                   context={'tags': tags})
 
 
-class PostDetail(View):
-    def get(self, request, slug):
-        # post = Post.objects.get(slug__iexact=slug)
-        post = get_object_or_404(Post, slug__iexact=slug )
-        return render(request, 'blog/post_detail.html',
-                      context={'post': post})
+class PostDetail(ObjectDetailMixin, View): # Post.mro() - покажет очереность поиска аотрибутов, важно в порядке использования наследования в классе
+    model = Post
+    template = 'blog/post_detail.html'
+    # def get(self, request, slug):
+    #     # post = Post.objects.get(slug__iexact=slug)
+    #     post = get_object_or_404(Post, slug__iexact=slug )
+    #     return render(request, 'blog/post_detail.html',
+    #                   context={'post': post})
 
 
-class TagDetail(View):
-    def get(self, request, slug):
-        # tag = Tag.objects.get(slug__iexact=slug)
-        tag = get_object_or_404(Post, slug__iexact=slug )
-        return render(request, 'blog/tag_detail.html',
-                      context={'tag': tag})
+class TagDetail(ObjectDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'
+    # def get(self, request, slug):
+    #     # tag = Tag.objects.get(slug__iexact=slug)
+    #     tag = get_object_or_404(Post, slug__iexact=slug )
+    #     return render(request, 'blog/tag_detail.html',
+    #                   context={'tag': tag})
 
+# миксины спец классы - от них наследуется часть логики
 
 # def post_detail(request, slug):  # slug придет из именнованой группы символов
 #     post = Post.objects.get(slug__iexact=slug)
