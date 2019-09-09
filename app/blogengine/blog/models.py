5ef64d5154ab455118b8d46bf66edba3bae54c37
alexe <alexe@W10SSD>
2019-09-10 00:49:08 +0300

    fix: неврное имя функции в путях

--- app/blogengine/blog/models.py
+++ app/blogengine/blog/models.py
@@ -12,8 +12,10 @@ class Post(models.Model):
     def __str__(self):
         return '{}'.format(self.title)
 
-    def get_absolute_url(self):  # метод необходим для упрощения работы с url
+    def get_absolute_url(self):  # метод необходим для упрощения работы с url (https://www.youtube.com/watch?v=4O1xdboP0PY&list=PLlWXhlUMyooaDkd39pknA1-Olj54HtpjX&index=5)
         return reverse("post_detail_url", kwargs={"slug": self.slug})
+        # первый аргумент название url шаблона
+        # post_detail_url - это название name='' в urls.py
 
 
 class Tag(models.Model):
@@ -22,3 +24,7 @@ class Tag(models.Model):
 
     def __str__(self):
         return '{}'.format(self.title)
+
+    def get_absolute_url(self):
+        return reverse("tag_detail_url", kwargs={"slug": self.slug})
+    
