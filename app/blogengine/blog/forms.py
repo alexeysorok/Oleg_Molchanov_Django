e0482606ad3a3c6b07dddfffb575e243713fe266
alexe <alexe@W10SSD>
2019-09-10 21:00:22 +0300

    добавли ModelForms

--- app/blogengine/blog/forms.py
+++ app/blogengine/blog/forms.py
@@ -3,20 +3,30 @@ from .models import *
 from django.core.exceptions import ValidationError
 
 
-class TagForm(forms.Form):
-    title = forms.CharField(max_length=50)  # CharField соответсвуте полю input
-    slug = forms.CharField(max_length=50)
+class TagForm(forms.ModelForm): # ModelForm позволяет сделаеть класс более универсальным за счет связывания с моделью
+    # title = forms.CharField(max_length=50)  # CharField соответсвуте полю input
+    # slug = forms.CharField(max_length=50)
 
-    # переопределяем для того чтобы задействовать на отображении bootstrap
-    title.widget.attrs.update({'class': 'form-control'})
-    slug.widget.attrs.update({'class': 'form-control'})
+    # # переопределяем для того чтобы задействовать на отображении bootstrap
+    # title.widget.attrs.update({'class': 'form-control'})
+    # slug.widget.attrs.update({'class': 'form-control'})
 
-    def save(self):
-        new_tag = Tag.objects.create(
-            title=self.cleaned_data['title'],
-            slug=self.cleaned_data['slug']
-        )
-        return new_tag
+    class Meta:
+        model = Tag
+        fields = ['title', 'slug']
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'slug': forms.TextInput(attrs={'class': 'form-control'}),
+
+        }
+
+    # def save(self):
+    #     new_tag = Tag.objects.create(
+    #         title=self.cleaned_data['title'],
+    #         slug=self.cleaned_data['slug']
+    #     )
+    #     return new_tag
 
     # пишем для какого поля clean_slug
     def clean_slug(self):
--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import *
+from .forms import *
 from django.views.generic import View
 
 from .utils import ObjectDetailMixin
@@ -51,3 +52,19 @@ class TagDetail(ObjectDetailMixin, View):
 #     tag = Tag.objects.get(slug__iexact=slug)
 #     return render(request, 'blog/tag_detail.html',
 #                   context={'tag': tag})
+
+
+class TagCreate(View):
+    def get(self, request):
+        form = TagForm()
+        return render(request, 'blog/tag_create.html',
+        context={'form': form})
+
+    def post(self, request):
+        bound_form = TagForm(request.POST)
+
+        if bound_form.is_valid():
+            new_tag = bound_form.save()
+            return redirect(new_tag)
+        return render(request, 'blog/tag_create.html',
+            context={'form': bound_form} )
