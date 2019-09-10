2b0ab624273d19ad8afd7cc3503ee11eebe9631c
alexe <alexe@W10SSD>
2019-09-10 20:52:18 +0300

    Работа с формами, первая форма

---
+++ app/blogengine/blog/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from .models import *
+from django.core.exceptions import ValidationError
+
+
+class TagForm(forms.Form):
+    title = forms.CharField(max_length=50)  # CharField соответсвуте полю input
+    slug = forms.CharField(max_length=50)
+
+    # переопределяем для того чтобы задействовать на отображении bootstrap
+    title.widget.attrs.update({'class': 'form-control'})
+    slug.widget.attrs.update({'class': 'form-control'})
+
+    def save(self):
+        new_tag = Tag.objects.create(
+            title=self.cleaned_data['title'],
+            slug=self.cleaned_data['slug']
+        )
+        return new_tag
+
+    # пишем для какого поля clean_slug
+    def clean_slug(self):
+        new_slug = self.cleaned_data['slug'].lower()
+
+        if new_slug == 'create':
+            raise ValidationError('Slug may not be "Create')
+        if Tag.objects.Filter(slug__iexact=new_slug).count():
+            raise ValidationError(
+                'Slug mast be unique. We have "{}" slug already'.format(new_slug))
+        return new_slug
