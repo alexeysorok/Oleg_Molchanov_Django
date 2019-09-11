7c8b04348c57908ac16d9ba70400681e2c278005
alexe <alexe@W10SSD>
2019-09-11 23:17:11 +0300

    Форма для поста

--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -68,3 +68,19 @@ class TagCreate(View):
             return redirect(new_tag)
         return render(request, 'blog/tag_create.html',
             context={'form': bound_form} )
+
+class PostCreate(View):
+    def get(self, request):
+        form = PostForm()
+        return render(request, 'blog/post_create.html',
+        context={'form': form})
+    
+    def post(self, request):
+        bound_form = PostForm(request.POST)
+        if bound_form.is_valid():
+            new_post = bound_form.save()
+            return redirect(new_post)
+        return render(request, 'blog/post_create.html',
+        context={'form': bound_form} )
+
+    
