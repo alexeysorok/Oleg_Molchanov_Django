051f0443267be494e5a0ff628d08a7188b86e603
alexe <alexe@W10SSD>
2019-09-12 18:21:54 +0300

    add: Панель из CSS

--- app/blogengine/blog/utils.py
+++ app/blogengine/blog/utils.py
@@ -10,7 +10,8 @@ class ObjectDetailMixin:
         # post = Post.objects.get(slug__iexact=slug)
         obj = get_object_or_404(self.model, slug__iexact=slug)
         return render(request, self.template,
-                      context={self.model.__name__.lower(): obj})
+                      context={self.model.__name__.lower(): obj, 'admin_object': obj,
+                      'detail': True})
 
 
 class ObjectCreateMixin:
